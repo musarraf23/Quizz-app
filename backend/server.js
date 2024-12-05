0f2f015c536b36615acfb0433f52499cdcfebac0
prashanthkumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2024-12-05 19:34:41 +0530

    backend done

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import {Server} from "node:http"
+
+const server = new Server((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    fetch('https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple')
+        .then(r=>r.json())
+        .then(({results})=>{
+            res.end(JSON.stringify(results));
+        });
+});
+
+server.listen(1000);
+
